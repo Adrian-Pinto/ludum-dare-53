b629f8ffd284a088363e4c0a688d9dc278877e8a
Marc Jourdes <marc.jourdes@king.com>
2023-05-01 00:03:31 +0200

    Add some sprites to 00 Add fog shaders Made souls bigger

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,14 +7,8 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public float offset;
     public float offsetSmoothing;
+    public float distance = -10.0f;
     private Vector3 playerPosition;
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-
-    }
 
     private void FixedUpdate()
     {
@@ -22,11 +16,11 @@ public class CameraController : MonoBehaviour
 
         if (player.transform.localScale.x > 0.0f)
         {
-            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, -10.0f);
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, distance);
         }
         else
         {
-            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, -10.0f);
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, distance);
         }
 
 
